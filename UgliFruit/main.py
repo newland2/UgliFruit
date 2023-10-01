770eac3e63f296d79c9db67a13aadfdde4b01b95
newland2 <enewland22@gmail.com>
2023-10-01 01:08:45 -0500

    adderoni pepperoni

--- UgliFruit/main.py
+++ UgliFruit/main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+import json
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -10,10 +11,14 @@ project = rf.workspace().project("ugli-fruit")
 model = project.version(4).model
 
 # infer on a local image
-print(model.predict("apple-single-red.jpg", confidence=40, overlap=30).json())
-
-# visualize your prediction
+temp = model.predict("apple-single-red.jpg", confidence=40, overlap=30).json()
+print(temp)
+fruit = json.dumps(model.predict("apple-single-red.jpg", confidence=40, overlap=30).json())
+fruitDict = json.loads(fruit)
+print(fruitDict['predictions'][0]['class'])
+# # visualize your pre
+# diction
 # model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
 
 # infer on an image hosted elsewhere
-# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+#print(model.predict("https://photos.google.com/u/1/photo/AF1QipM0kFBxG71Cco14FvjmnDmqKYMqhcnEk3ekPr7I", hosted=True, confidence=40, overlap=30).json())
