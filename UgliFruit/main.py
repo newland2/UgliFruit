ef9d3a84a87ee6c33dad4374dab6cb1a923078c5
newland2 <enewland22@gmail.com>
2023-10-01 01:13:10 -0500

    added index

--- UgliFruit/main.py
+++ UgliFruit/main.py
@@ -11,8 +11,7 @@ project = rf.workspace().project("ugli-fruit")
 model = project.version(4).model
 
 # infer on a local image
-temp = model.predict("apple-single-red.jpg", confidence=40, overlap=30).json()
-print(temp)
+
 fruit = json.dumps(model.predict("apple-single-red.jpg", confidence=40, overlap=30).json())
 fruitDict = json.loads(fruit)
 print(fruitDict['predictions'][0]['class'])
